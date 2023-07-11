269a51de0cdd549cd0f32a4a3409306bfe4f53aa
olvericc <olv.ericlopes@gmail.com>
2023-07-10 21:24:31 -0300

    project: created

---
+++ src/main/java/com/devolveric/dslistv2/Dslistv2Application.java
@@ -0,0 +1,13 @@
+package com.devolveric.dslistv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dslistv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dslistv2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/devolveric/dslistv2/Dslistv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devolveric.dslistv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dslistv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
