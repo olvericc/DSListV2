6db47485aadf5b569b55a733e820f334404f591b
olvericc <olv.ericlopes@gmail.com>
2023-07-10 21:44:04 -0300

    project: create Fruit class and config h2 database for test profile

---
+++ src/main/java/com/devolveric/dslistv2/entities/Fruit.java
@@ -0,0 +1,92 @@
+package com.devolveric.dslistv2.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table (name = "fruits")
+public class Fruit {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer quantity;
+    private String tasteDescription;
+    private String color;
+    private String type;
+
+    public Fruit() {
+    }
+
+    public Fruit(Long id, String name, Integer quantity, String tasteDescription,
+                 String color, String type) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.tasteDescription = tasteDescription;
+        this.color = color;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getTasteDescription() {
+        return tasteDescription;
+    }
+
+    public void setTasteDescription(String tasteDescription) {
+        this.tasteDescription = tasteDescription;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fruit fruit = (Fruit) o;
+        return Objects.equals(id, fruit.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
