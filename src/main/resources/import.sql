afcbd6f8cb1f2d565894d6bf73e6ceba8c0c9563
eric.oliveira <olv.ericlopes@gmail.com>
2023-07-12 14:08:50 -0300

    project: adding new sql inserts on h2 database (belonging and fruit_list)

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,6 @@
+INSERT INTO fruit_list (id, name) VALUES (1, 'Juicy Fruits');
+INSERT INTO fruit_list (id, name) VALUES (2, 'Sour Fruits');
+
 INSERT INTO fruits (id, name, quantity, taste, color, type) VALUES (1, 'Apple', 5, 'Sweet and tangy', 'Red', 'Fruit');
 INSERT INTO fruits (id, name, quantity, taste, color, type) VALUES (2, 'Orange', 3, 'Citrusy', 'Orange', 'Fruit');
 INSERT INTO fruits (id, name, quantity, taste, color, type) VALUES (3, 'Banana', 7, 'Sweet', 'Yellow', 'Fruit');
@@ -8,6 +11,7 @@ INSERT INTO fruits (id, name, quantity, taste, color, type) VALUES (7, 'Pineappl
 INSERT INTO fruits (id, name, quantity, taste, color, type) VALUES (8, 'Mango', 6, 'Sweet and tropical', 'Yellow', 'Fruit');
 INSERT INTO fruits (id, name, quantity, taste, color, type) VALUES (9, 'Raspberry', 9, 'Sweet and tangy', 'Red', 'Fruit');
 INSERT INTO fruits (id, name, quantity, taste, color, type) VALUES (10, 'Plum', 3, 'Juicy and sweet', 'Purple', 'Fruit');
+
 INSERT INTO sellers (id, first_name, last_name, email, phone) VALUES (1, 'João', 'Silva', 'joao.silva@example.com', '1234567890');
 INSERT INTO sellers (id, first_name, last_name, email, phone) VALUES (2, 'Maria', 'Santos', 'maria.santos@example.com', '9876543210');
 INSERT INTO sellers (id, first_name, last_name, email, phone) VALUES (3, 'Carlos', 'Lima', 'carlos.lima@example.com', '5555555555');
@@ -18,3 +22,15 @@ INSERT INTO sellers (id, first_name, last_name, email, phone) VALUES (7, 'Ricard
 INSERT INTO sellers (id, first_name, last_name, email, phone) VALUES (8, 'Mariana', 'Rodrigues', 'mariana.rodrigues@example.com', '8888888888');
 INSERT INTO sellers (id, first_name, last_name, email, phone) VALUES (9, 'Luís', 'Fernandes', 'luis.fernandes@example.com', '4444444444');
 INSERT INTO sellers (id, first_name, last_name, email, phone) VALUES (10, 'Teresa', 'Gomes', 'teresa.gomes@example.com', '6666666666');
+
+INSERT INTO belonging (list_id, fruit_id, position) VALUES (1, 1, 0);
+INSERT INTO belonging (list_id, fruit_id, position) VALUES (1, 2, 1);
+INSERT INTO belonging (list_id, fruit_id, position) VALUES (1, 3, 2);
+INSERT INTO belonging (list_id, fruit_id, position) VALUES (1, 4, 3);
+INSERT INTO belonging (list_id, fruit_id, position) VALUES (1, 5, 4);
+
+INSERT INTO belonging (list_id, fruit_id, position) VALUES (2, 6, 0);
+INSERT INTO belonging (list_id, fruit_id, position) VALUES (2, 7, 1);
+INSERT INTO belonging (list_id, fruit_id, position) VALUES (2, 8, 2);
+INSERT INTO belonging (list_id, fruit_id, position) VALUES (2, 9, 3);
+INSERT INTO belonging (list_id, fruit_id, position) VALUES (2, 10, 4);
