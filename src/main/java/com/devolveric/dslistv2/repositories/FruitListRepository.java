d37afb24c3aef09a53343c16b7344b4fe2a09258
olvericc <olv.ericlopes@gmail.com>
2023-07-16 17:08:41 -0300

    project: adding new endpoint @PostMapping to move fruits on list

--- src/main/java/com/devolveric/dslistv2/repositories/FruitListRepository.java
+++ src/main/java/com/devolveric/dslistv2/repositories/FruitListRepository.java
@@ -2,5 +2,25 @@ package com.devolveric.dslistv2.repositories;
 
 import com.devolveric.dslistv2.entities.FruitList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
-public interface FruitListRepository extends JpaRepository<FruitList, Long> { }
+public interface FruitListRepository extends JpaRepository<FruitList, Long> {
+
+    @Modifying
+    @Query(
+            nativeQuery = true,
+            value =
+                    """
+            UPDATE 
+                belonging
+            SET
+                position = :newPosition
+            WHERE
+                list_id :listId
+            AND
+                fruit_id = :fruitId
+            """)
+    static void updateBelongingPosition(Long listId, Long fruitId, Integer newPosition) {
+    }
+}
